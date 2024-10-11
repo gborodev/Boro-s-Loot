12474abf4af5e1f36a506e9690d71891989946ed
Kaan Yağlıdere <gainsborodev@gmail.com>
2024-10-11 21:58:13 +0300

    Started Enemy Stats

---
+++ Assets/Scripts/Database Scripts/DataSO.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class DataSO : ScriptableObject
+{
+    [Header("Data Stats")]
+    private string _id;
+
+    [SerializeField] private string _name;
+
+    public string ID => _id;
+    public string Name => _name;
+
+    protected virtual DataSO GetInstance()
+    {
+        _id = Guid.NewGuid().ToString();
+
+        return this;
+    }
+}
---
+++ Assets/Scripts/Database Scripts/EnemySO.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Enemy Data")]
+public class EnemySO : DataSO
+{
+    [Header("Enemy Stats")]
+    [SerializeField] private int _health;
+    [SerializeField] private int _damage;
+
+    private EnemyStat[] stats;
+
+    private float GetStatValue(EnemyStatType statType, float multiplier)
+    {
+        return stats[(int)statType].GetValue(multiplier);
+    }
+
+    protected override DataSO GetInstance()
+    {
+        stats = new EnemyStat[]
+        {
+            new EnemyStat(EnemyStatType.Health, _health),
+            new EnemyStat(EnemyStatType.Damage, _damage)
+        };
+
+        return Instantiate(base.GetInstance());
+    }
+}
+
+public enum EnemyStatType { Health, Damage }
+
+public struct EnemyStat
+{
+    private EnemyStatType statType;
+    private float baseValue;
+
+    public EnemyStat(EnemyStatType statType, float baseValue)
+    {
+        this.statType = statType;
+        this.baseValue = baseValue;
+    }
+
+    public float GetValue(float multiplier)
+    {
+        return baseValue * multiplier;
+    }
+}
